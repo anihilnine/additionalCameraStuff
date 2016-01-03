3ebfbc5a3ac07d8080cb1353c30d90dee66a5669
Bra1nwash3d <Bra1nwash3d@users.noreply.github.com>
2016-01-03 18:17:18 +0100

    using a better hook for worldview

--- hook/lua/ui/controls/worldview.lua
+++ hook/lua/ui/controls/worldview.lua
@@ -7,31 +7,7 @@ WorldView = Class(oldWorldView, Control) {
         if (not self.isZoom) and (event.Type == 'WheelRotation') then
             return true
         end
-        if self.EventRedirect then
-            return self.EventRedirect(self,event)
-        end
-        if event.Type == 'MouseEnter' or event.Type == 'MouseMotion' then
-            self.bMouseIn = true
-            if self.Cursor then
-                if (self.LastCursor == nil) or (self.Cursor[1] != self.LastCursor[1]) then
-                    self.LastCursor = self.Cursor
-                    GetCursor():SetTexture(unpack(self.Cursor))
-                end
-            else
-                GetCursor():Reset()
-            end
-        elseif event.Type == 'MouseExit' then
-            self.bMouseIn = false
-            GetCursor():Reset()
-            self.LastCursor = nil
-            if self.TargetDecal then
-                self.TargetDecal:Destroy()
-                self.TargetDecal = false
-                self.DecalTexture = false
-                self.DecalScale = false
-            end
-        end
-        return false
+        return oldWorldView.HandleEvent(self, event)
     end,
 
     SetAllowZoom = function(self, bool)
